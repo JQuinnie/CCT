021b3dcf9c2129740cbc1fc104a5d31625862527
JQuinnie <jenn8866@gmail.com>
2018-01-19 11:16:59 -0600

    create js to set up routes for displaying and saving data to the db

---
+++ routes/api-routes.js
@@ -0,0 +1,24 @@
+// Set up routes for displaying and saving data to the db
+
+// Setting up dependencies
+var db = require('../models');
+
+// Setting up routes
+module.exports = function (app) {
+
+  // GET route for getting all of the different cryptocoin data
+  //db.findAll
+
+  // GET route for retrieving data on a single cyptocoin
+  //db.findOne
+
+  // GET route for retrieving data of all cyptocoins a user is tracking
+  //db.findAll where:query include:userId
+
+  // POST/PUT route for saving a cyptocoin for user to track
+  //db.create
+
+  // DELETE route for deleting a cyptocoin for user to track
+  //db.destroy
+
+}
