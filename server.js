f62daf0d97b19db42e1e879b7c1440186fcbf85d
Fernando Fuentes <fernando.fuentes1113@gmail.com>
2018-01-20 00:48:18 -0600

    my path is messed up - this is a test commit

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ require('./routes/api-routes.js')(app);
 // Listening on PORT, Syncing Sequelize models and starting Express app
 db.sequelize.sync({
   force: true
-}).then(function () {
-  app.listen(PORT, function () {
+}).then(function() {
+  app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 });
