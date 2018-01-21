7480bb134977dc0b1cc569bc75ec78d7eba29d0d
Fernando Fuentes <fernando.fuentes1113@gmail.com>
2018-01-21 10:50:00 -0600

    Links

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,9 +4,21 @@
 var path = require('path');
 
 // setting up Routes
-module.exports = function (app) {
+module.exports = function(app) {
   // index route loads index.handlebars
-  app.get('/', function (req, res) {
+  app.get('/', function(req, res) {
     res.render('index');
   })
-}
+
+  app.get("/user", function(req, res) {
+    res.render("user");
+  })
+
+  app.get("/login", function(req, res) {
+    res.render("login");
+  })
+
+  app.get("/registration", function(req, res) {
+    res.render("registration");
+  })
+};
