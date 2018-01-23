f9905e4e31ba46fc09f7c9dac1303df80839bc0e
fernandofuentes <fernando.fuentes1113@gmail.com>
2018-01-22 23:17:07 -0600

    Handlebars and CSS

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '!!!!!INSERT!!!!!',
+    password: 'mysqlpw!',
     database: 'crypto_db'
   })
 }
