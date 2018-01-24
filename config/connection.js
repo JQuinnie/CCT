ce5734bb5c11f669d43cf7f8f41617502c3ee53d
Batli Joselevitz <batlijoselevitz@Batlis-MacBook-Pro.local>
2018-01-23 18:22:16 -0600

    created homepage wirefram and start test 1

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '!!!!!INSERT!!!!!',
+    password: 'santal33!',
     database: 'crypto_db'
   })
 }
