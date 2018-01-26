5a1656ea3904b75fd717a7628ddad9c243ecddd1
Fernando Fuentes <fernando.fuentes1113@gmail.com>
2018-01-25 20:18:04 -0600

    heroku deploy and sucks

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'santal33!',
+    password: 'sostfffs2FF!',
     database: 'crypto_db'
   })
 }
