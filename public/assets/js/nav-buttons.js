da594a00490b14e186dfbd807475cd3e654e616f
fernandofuentes <fernando.fuentes1113@gmail.com>
2018-01-26 21:25:00 -0600

    class update on the buttons

--- public/assets/js/nav-buttons.js
+++ public/assets/js/nav-buttons.js
@@ -1,12 +1,18 @@
 // NAV BUTTONS
 $(document).ready(function() {
   if (userData.firstname) {
-    $(".btn-logout").show();
+    // Logoutbutton
+    $(".btn-danger").show();
+    // Registration button
     $(".btn-btn-primary").hide();
+    // SignIn BUTTONS
     $(".btn-success").hide();
   } else {
-    $(".btn-logout").hide();
+    // Logoutbutton
+    $(".btn-danger").hide();
+    // Registration button
     $(".btn-btn-primary").show();
+    // SignIn BUTTONS
     $(".btn-success").show();
   }
 })
